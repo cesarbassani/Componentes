edb804b5c3a3c7094e6686e30df89eeaa5f0dc21
Cesar Bassani <cesarbassani@Cesars-MacBook-Pro.local>
2018-04-18 17:09:10 -0400

    Customizando Toast Notification

--- app/src/main/java/com/cesarbassani/myapplication/MainActivity.java
+++ app/src/main/java/com/cesarbassani/myapplication/MainActivity.java
@@ -1,9 +1,13 @@
 package com.cesarbassani.myapplication;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -32,7 +36,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int id = v.getId();
 
         if (id == R.id.button_toast){
-            Toast.makeText(this, R.string.toast_me, Toast.LENGTH_SHORT).show();
+            Toast toast = Toast.makeText(this, R.string.toast_me, Toast.LENGTH_SHORT);
+
+            LayoutInflater inflater = getLayoutInflater();
+            View toastLayout = inflater.inflate(R.layout.toast_layout, (ViewGroup) findViewById(R.id.linear_toast_custom_layout)) ;
+            toast.setView(toastLayout);
+
+            TextView textView = toast.getView().findViewById(R.id.text_toast);
+            textView.setTextColor(Color.BLUE);
+
+//            View view = toast.getView();
+//            view.setBackgroundColor(Color.BLACK);
+
+            toast.show();
+
         }
     }
 
